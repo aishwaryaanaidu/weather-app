dc400bb03e9b196e62a551e98921aacbc7d8d7fa
unknown <aishwarya13naidu@gmail.com>
2020-08-12 20:48:40 +0530

    Adding HTML

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const express = require('express');
 // Creating an instance named app invoking express
 const app = express();
 
-// If we visit the root URL, Express will respond with “Hello World!”.
+// allows access to all of the static files within the ‘public’ folder
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+// If we visit the root URL, Express will respond with the content inside.
 app.get('/', function(req, res) {
-    res.send('Hello World!!!')
+    // res.send('Hello World!!!')
+
+    // Render the view and then send equivalent HTML to the client
+    res.render('index');
 })
 
 // The server is listening on port 3000 for connections
